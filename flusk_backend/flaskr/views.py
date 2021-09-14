33a0b3db0fc0bf02174a842803cf37f6f4b6e0f2
Lukas <lukkaempf@gmail.com>
2021-09-14 22:51:15 +0200

    Login System Update

--- flusk_backend/flaskr/views.py
+++ flusk_backend/flaskr/views.py
@@ -11,6 +11,11 @@ views= Blueprint('views', __name__)
 
 secondsecretkey = 'donthack'
 
+def check_token(token):
+    token_decode = jwt.decode(token, secondsecretkey)
+    return token_decode
+
+
 
 def token_required(f):
     @wraps(f)
@@ -24,7 +29,7 @@ def token_required(f):
             return jsonify({'message': 'token is missing'}), 403
 
         try:
-            data = jwt.decode(token, secondsecretkey)
+            data = check_token(token)
             current_user = Users.query.filter_by(username=data['username']).first()
         except:
             return jsonify({'message':'token is invalid'}), 403
@@ -32,20 +37,27 @@ def token_required(f):
     return decorated
 
 
-@views.route('/api/login/', methods=['GET','POST'])
+@views.route('/api/login', methods=['GET','POST'])
 def home():
     if request.method == 'POST':
         requestdata = (request.form).to_dict(flat=False)
         username = ''.join(requestdata['username'])
         passwort = ''.join(requestdata['password'])
+        print(username)
+        if not username and not passwort:
+            return jsonify({'message':'Geben Sie eine Email and Passwort ein.'}), 403
+        
         user = db.session.query(Users).filter(Users.username==username).first()
         if user:
             if passwort == user.password:
                 token = jwt.encode({'username':user.username,"exp": datetime.now() + timedelta(minutes=30)}, secondsecretkey)
                 return jsonify({'token': token.decode('UTF-8')})
-            return jsonify({'message':'Benutzername oder Passwort falsch'})
-        return jsonify({'message':'Benutzername oder Passwort falsch'})
-    return jsonify({'message':'test'})
+            return jsonify({'message':'Benutzername oder Passwort falsch.'}), 403
+        return jsonify({'message':'Benutzername oder Passwort falsch.'}), 403
+    return jsonify(
+        color='bamboo',
+        left_handed=True
+    )
 
 @views.route('/api/test/')
 @token_required
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
-import 'dart:html';
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:convert';
+
+final storage = FlutterSecureStorage();
 
 void main() {
   runApp(MyApp());
+}
+
+Future<String> loginRequired(BuildContext context) async {
+  String? token = await storage.read(key: 'token');
+  if (token == null) {
+    Navigator.pushReplacementNamed(context, 'login');
+  }
+  return token.toString();
+}
+
+Future<void> logout(context) async {
+  await storage.delete(key: 'token');
+  Navigator.of(context).pushReplacementNamed('login');
 }
 
 class MyApp extends StatelessWidget {
@@ -18,24 +35,69 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Home(),
+      routes: {'home': (context) => Home(), 'login': (context) => Login()},
     );
   }
 }
 
 class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    loginRequired(context);
+  }
+
+/*   @override
+  void dispose() {
+    super.dispose();
+  } */
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text('test'),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          FloatingActionButton(
+            onPressed: () => logout(context),
+            child: Text('Abmelden'),
+            heroTag: '1',
+          ),
+          FloatingActionButton(
+            onPressed: () => test1(),
+            child: Text('Test'),
+            heroTag: '2',
+          )
+        ],
+      )),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
-
-  Color maincolor = Color(0xffe9e9e9);
-
-  Color shadowcolor = Color.fromRGBO(0, 0, 0, .3);
-
   String _username = '';
   String _password = '';
+  String? errorMessage = '';
+  bool _isObscure = true;
+
+  Color maincolor = Color(0xffe9e9e9);
+  Color shadowcolor = Color.fromRGBO(0, 0, 0, .3);
 
   @override
   Widget build(BuildContext context) {
@@ -52,22 +114,22 @@ class _HomeState extends State<Home> {
               children: [
                 Container(
                   child: TextFormField(
-                      validator: (value) =>
-                          (value?.isEmpty ?? true ? 'Enter an email' : null),
                       onSaved: (String? value) => _username = value!,
                       cursorColor: Colors.black,
                       decoration: InputDecoration(
-                          hintText: 'Benutzername',
-                          fillColor: maincolor,
-                          filled: true,
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: maincolor),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: maincolor),
-                            borderRadius: BorderRadius.circular(15),
-                          ))),
+                        errorText: errorMessage,
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: maincolor),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: maincolor),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        hintText: 'Benutzername',
+                        fillColor: maincolor,
+                        filled: true,
+                      )),
                   decoration: BoxDecoration(boxShadow: [
                     BoxShadow(
                       color: shadowcolor,
@@ -81,23 +143,40 @@ class _HomeState extends State<Home> {
                 ),
                 Container(
                   child: TextFormField(
-                      validator: (value) => (value?.isEmpty ?? true
-                          ? 'Enter the passwort'
-                          : null),
+                      /* validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Passwort Feld leer';
+                        } else if (errorMessage.isNotEmpty) {
+                          return errorMessage;
+                        }
+                        return null;
+                      }, */
+                      obscureText: _isObscure,
                       onSaved: (String? value) => _password = value!,
                       cursorColor: Colors.black,
                       decoration: InputDecoration(
-                          hintText: 'Passwort',
-                          fillColor: maincolor,
-                          filled: true,
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: maincolor),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: maincolor),
-                            borderRadius: BorderRadius.circular(15),
-                          ))),
+                        suffixIcon: IconButton(
+                            icon: Icon(_isObscure
+                                ? Icons.visibility
+                                : Icons.visibility_off),
+                            onPressed: () {
+                              setState(() {
+                                _isObscure = !_isObscure;
+                              });
+                            }),
+                        errorText: errorMessage,
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: maincolor),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: maincolor),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        hintText: 'Passwort',
+                        fillColor: maincolor,
+                        filled: true,
+                      )),
                   decoration: BoxDecoration(boxShadow: [
                     BoxShadow(
                       color: shadowcolor,
@@ -112,7 +191,32 @@ class _HomeState extends State<Home> {
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: RaisedButton(
-                        onPressed: _submit,
+                        onPressed: () async {
+                          errorMessage = '';
+                          _formKey.currentState!.save();
+                          Map map = {
+                            'username': _username,
+                            'password': _password
+                          };
+
+                          Map data = await login(context, map);
+                          setState(() {
+                            errorMessage = data['message'];
+                          });
+
+                          /* foo(map);
+                            FutureBuilder(
+                                future: user,
+                                builder: (context, snapshot) {
+                                  print(snapshot.data);
+                                  if (snapshot.hasData) {
+                                    print('df');
+                                    return Text('err');
+                                  } else {
+                                    return Text('test');
+                                  }
+                                }); */
+                        },
                         child: Text('Sign in'),
                       ),
                     )
@@ -125,36 +229,39 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+}
 
-  void _submit() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      print(_username);
-      print(_password);
-      Map map = {'username': _username, 'password': _password};
-      test();
-      //login(map);
-    }
+login(context, data) async {
+  //context, String username, String password, final formKey
+  var url = Uri.parse('http://10.0.2.2:5000/api/login');
+  var result = await http.post(url, body: data);
+  if (result.statusCode != 200) {
+    return json.decode(result.body);
   }
+
+  //String? value = await storage.read(key: 'token');
+
+  test(context, result);
+  return json.decode(result.body);
 }
 
-Future<void> login(Map listdata) async {
-  print(listdata);
-  var url = Uri.parse('http://localhost:5000/api/login');
-  var result = await http.post(url, body: listdata);
+test(context, result) async {
+  Map<String, dynamic> resultToJson = json.decode(result.body);
+  await storage.write(key: 'token', value: resultToJson['token']);
+  Navigator.pushReplacementNamed(context, 'home');
+}
+
+Future<String> test1() async {
+  var url = Uri.parse('http://10.0.2.2:5000/api/test/');
+
+  //final token =
+  //  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNjMwNzE1MjU3fQ.iY-_UBE2NxlG5MTTtydWW01m1TGBOnMIGZfwG80Kjik';
+  final token = await storage.read(key: 'token');
+
+  var result = await http.get(url, headers: {
+    'x-access-token': token.toString(),
+    'Content-type': 'application/json'
+  });
   print(result.body);
+  return result.body.toString();
 }
-
-Future<void> test() async {
-  var url = Uri.parse('http://localhost:5000/api/test');
-
-  final token = 'WIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikpv';
-  //+final headers = {HttpHeaders.authorizationHeader: token};
-
-  var result =
-      await http.get(url, headers: {HttpHeaders.authorizationHeader: token});
-  //print(result);
-}
-
-//HttpHeaders.authorizationHeader:
-      
